52a08dc3770647c745d3b6e42411a82416e2b800
bpshparis <iic_paris@fr.ibm.com>
2020-05-12 12:32:50 +0200

    ...

---
+++ scripts/getVMAddress.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for LINE in $(vim-cmd vmsvc/getallvms | awk '{if (NR > 1) print $1 ";" $2}'); do
+	VMID=$(echo $LINE | cut -d ";" -f1);
+	VMNAME=$(echo $LINE | cut -d ";" -f2);
+	#VMIP=$(vim-cmd vmsvc/get.guest $VMID | grep -o -Ei -m 1 'ipaddress = "([^\"])*"' | awk -F "\"" '{print $2}')
+	VMIP=$(vim-cmd vmsvc/get.guest $VMID | grep -i -m 1 'ipaddress = "172.16.1' | awk -F "\"" '{print $2}')
+	case $VMNAME in m*) echo $VMID";"$VMNAME";"$VMIP;; esac
+	case $VMNAME in w*) echo $VMID";"$VMNAME";"$VMIP;; esac
+	case $VMNAME in lb*) echo $VMID";"$VMNAME";"$VMIP;; esac
+	case $VMNAME in bs*) echo $VMID";"$VMNAME";"$VMIP;; esac
+done
+
